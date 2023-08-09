3117248e959652543e05dfb17bbd84a1994377dd
an-ku-sh <110815021+an-ku-sh@users.noreply.github.com>
2023-08-09 14:27:51 +0530

    Init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {}
+
+class MaterialAppBase extends StatefulWidget {
+  const MaterialAppBase({super.key});
+
+  @override
+  State<MaterialAppBase> createState() => _MaterialAppBaseState();
+}
+
+class _MaterialAppBaseState extends State<MaterialAppBase> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
