c4f3d44f79c0a2c022044b4c17ba6c2f005b4f12
an-ku-sh <110815021+an-ku-sh@users.noreply.github.com>
2023-09-14 23:46:23 +0530

    std

--- lib/Widgets/std_tile.dart
+++ lib/Widgets/std_tile.dart
@@ -21,57 +21,60 @@ class StdTile extends StatelessWidget {
           MaterialPageRoute(builder: (context) => const ProfilePage()),
         );
       },
-      child: Container(
-        margin: EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.grey[200],
-          boxShadow: [
-            BoxShadow(
-                blurRadius: 15.0,
-                offset: const Offset(6, 6),
-                spreadRadius: 1,
-                color: Colors.grey.shade500),
-            const BoxShadow(
-                blurRadius: 5.0,
-                offset: Offset(-6, -6),
-                spreadRadius: 1,
-                color: Colors.white)
-          ],
-        ),
-        child: Center(
+      child: Expanded(
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey[200],
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 15.0,
+                  offset: const Offset(6, 6),
+                  spreadRadius: 1,
+                  color: Colors.grey.shade500),
+              const BoxShadow(
+                  blurRadius: 5.0,
+                  offset: Offset(-6, -6),
+                  spreadRadius: 1,
+                  color: Colors.white)
+            ],
+          ),
           child: Center(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                    ),
-                    Text(
-                      stdName,
-                      style: TextStyle(
-                        color: Color(0xFF2E4090),
-                        fontSize: 20,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                    ),
-                    Text(
-                      rollNo,
-                      style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w500,
-                          color: const Color(0xFF5A5D72),
-                          fontSize: 15),
-                    ),
-                  ],
-                ),
-              ],
+                      Text(
+                        stdName,
+                        style: TextStyle(
+                          color: Color(0xFF2E4090),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Text(
+                        rollNo,
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xFF5A5D72),
+                            fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
