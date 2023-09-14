7d7e7297e11abde3c78a8a46306dfb311cb1aa53
an-ku-sh <110815021+an-ku-sh@users.noreply.github.com>
2023-09-14 21:46:04 +0530

    awd

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Add arrays here
   @override
   Widget build(BuildContext context) {
     return Scaffold(
